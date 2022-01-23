55edd7dc1ece6662288e90d7bbfb6aba3f41d977
katechup <97139080+ktln-grace@users.noreply.github.com>
2022-01-23 18:35:53 +0800

    User-copy

---
+++ 2-AboutUs_user.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow&family=Fira+Sans&family=Francois+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/7d61347327.js" crossorigin="anonymous"></script>
+    <title>About Us | City Word Family Church Inc.</title>
+</head>
+<body>
+    <header>
+        <div class="info">
+            <ul class="info-links">
+                <li><a href="#">+639398517336</a></li>
+                <li><a href="#">cityword20@gmail.com</a></li>
+            </ul>
+        </div>
+        <div class="icons">
+            <a href="https://www.facebook.com/CovenantWorkersForChrist"><i id="fb" class="fab fa-facebook-square"></i></a>
+            <a href="https://www.youtube.com/channel/UCH68AOKb2QyEga571FVBQIw"><i id="yt" class="fab fa-youtube"></i></a>
+        </div>
+    </header>
+
+    <!-- Navigation Bar -->
+    <nav class="navbar">
+        <div class="logo">
+            <a href="index.php"><img src="logo/Logo.png" id="logo"></a>
+        </div>
+        <ul class="nav-links">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="2-AboutUs.php">About Us</a></li>
+            <li><a href="3-Preachings.php">Preachings</a></li>
+            <li><a href="#">Ministries</a></li>
+            <li><a href="5-Give.php">Give</a></li>
+            <li><a href="6-CnnctWthS.php">Connect with Us</a></li>
+            <li><a href="#">|</a></li>
+            <li><a href="logIn.php">Log-in</a></li>
+        </ul>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>   
+        </div>
+    </nav> 
+
+    <!-- 1 -->
+    <section class="aboutus">
+        <div class="content">
+                <p class="title"> About Us </p>
+                <h4> Existing in Obedience to the</h4>
+                <h3> mandate of Christ</h3>
+        </div>
+    </section>
+
+    <section class="add">
+        <!-- mission -->
+        <div class ="container">
+            <div class="box">
+            <div class="icon">
+                <a href="#"><img src="pictures/icon1.png"></a>
+            </div>
+                <div class="content">
+                <h3>MISSION</h3>
+                <p class ="content1">We exist in obedience to the mandate of Christ to His Church (the great commission) that privilege us to evangelize, edify, equip, and empower.</p>
+                </div>
+            </div>
+        </div>
+        <!-- vision -->
+        <div class ="container">
+            <div class="box">
+            <div class="icon">
+                <a href="#"><img src="pictures/icon2.png"></a>
+            </div>
+                <div class="content">
+                <h3>VISION</h3>
+                <p class = "content2">We see a humble and loving church, united for His purpose where every member is a minister. To function for His glory that mobilizes His body and in-partnership with the work of Christ in reaching the unreached and saving the unsaved. </p>
+                </div>
+            </div>
+        </div>
+        <!-- core values -->
+        <div class ="container">
+            <div class="box">
+            <div class="icon">
+                <a href="#"><img src="pictures/icon3.png"></a>
+            </div>
+                <div class="content">
+                <h3>CORE VALUES</h3>
+                <p class ="3">a. Biblical Living<br/>
+                Dependent Prayer, Persistent Growth, Generous Stewardship
+                <br/><br/>
+                b. Relational Community<br/>
+                Authentic relationship, Sacrificial Ministry, Extreme Hospitality
+                <br/><br/>
+                c. Intentional Outreach<br/> 
+                Mission Living ,Gospel Proclamation, Global Impact 
+                </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- 2 -->
+    <section class="wwd">
+        <div class="container1">
+        <h1> What we do?</h1>
+        <p class="content"> Discipleship, Training, Equipping, Counseling, Kairos Seminar, Social Work, etc.</p>
+        <div class="wrapper">
+        <div class="card">
+            <img src="pictures/wwd1.png">
+            <div class="info">
+                <h1>Equipping</h1>
+                <p>And He Himself gave some to be apostles, some prophets, some evangelists, and some pastors and teachers, for the equipping of the saints for the work of ministry, for the [a]edifying of the body of Christ, <br><br> Ephesians 4:11-12</p>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="pictures/wwd2.png">
+            <div class="info">
+                <h1>Discipleship</h1>
+                <p>Therefore go and make disciples of all nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit, <br><br> Matthew 28:19 </p>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="pictures/wwd3.png">
+            <div class="info">
+                <h1>Social Work</h1>
+                <p>Who would you rather be: the one who eats the dinner or the one who serves the dinner? You’d rather eat and be served, right? But I’ve taken my place among you as the one who serves. <br><br> Luke 22:27</p>
+            </div>
+        </div>
+    </div>
+        </div>
+    </section>
+
+    <!-- 3 -->
+    <section class="statement">
+        <div class="content">
+            <h3> Our</h3>
+            <h1> Statement of Faith </h1>
+            <p> We Believe . . . <br> <br>
+                1. The Scriptures, both Old and New Testaments, to be the inspired Word of God, without error in the original writing, the complete revelation of His will for the salvation of men, and the Divine and final authority for all Christian faith and life. <br> <br>
+                2. In one God, Creator of all things, infinitely perfect and eternally existing in three persons, Father, Son and Holy Spirit. <br> <br>
+                3. That Jesus Christ is true God and true man, having been conceived of the Holy Spirit and born of the virgin Mary. He died on the Cross a sacrifice for our sins according to the Scriptures. Further, He arose bodily from the dead, ascended into heaven, where, at the right hand of the Majesty on High He now is our High Priest and advocate. <br> <br>
+                4. That the ministry of the Holy Spirit is to glorify the Lord Jesus Christ, and during this age, to convict men, regenerate the believing sinner, indwell, guide, instruct, and empower the believer for Godly living and service. <br> <br>
+                5. That man was created in the image of God, but fell into sin and is therefore lost and only through regeneration by the Holy Spirit can salvation and spiritual life be obtained. <br> <br>
+                6. That the shed blood of Jesus Christ and His resurrection provide the only ground for justification and salvation for all who believe, and only such as receive Jesus Christ are born of the Holy Spirit, and thus become Children of God. <br> <br>
+                7. That water baptism and the Lord’s Supper are ordinances to be observed by the church during the present age. They are, however, not to be regarded as a means of salvation. <br> <br>
+                8. That the true Church is composed of all such persons, who, through saving faith in Jesus, have been regenerated by the Holy Spirit and are united together in the body of Christ of which He is the head. <br> <br>
+                9. That only those who are thus members of the true Church shall be eligible for membership in the local church. <br> <br>
+                10. That Jesus Christ is the Lord and Head of the church, and that every local church has the right under Christ to decide and govern its own affairs. <br> <br>
+                11. In the personal and pre-millennial and imminent coming of our Lord Jesus Christ and that this “Blessed Hope” has a vital bearing on the personal life and service of the believer. <br> <br>
+                12. In the bodily resurrection of the dead; of the believer to everlasting blessedness and joy with the Lord, of the unbeliever to judgment and everlasting conscious punishment. <br> <br>
+</p>
+        </div>
+    </section>
+
+    
+    <!-- Footer -->
+    <section class="footer">
+    <div class="container flex">
+        <div class="logo">
+            <a href="index.php"><img src="logo/foot-logo.png" id="logo"></a>
+        </div>
+        <div class="foot-links">
+            <div>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="2-AboutUs.php">About Us</a></li>
+                    <li><a href="3-Preachings.php">Preachings</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#">Ministries</a></li>
+                    <li><a href="5-Give.php">Give</a></li>
+                    <li><a href="6-CnnctWthS.php">Connect with Us</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="info">
+            <p> 10th Swallow Street Francisville subd. Mambugan, Antipolo City PH <br> <br>
+            cityword20@gmail.com <br> <br>
+            (+63) 9398517336 </p>   
+        </div>
+    </div>
+    </section>
+
+    <footer class="foot">
+            <p> City Word Family Church 2022</p>
+    </footer>
+
+        <script src="app.js"></script>
+</body>
+</html>
---
+++ 3-Preachings_user.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow&family=Fira+Sans&family=Francois+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/7d61347327.js" crossorigin="anonymous"></script>
+    <title>Preaching | City Word Family Church Inc.</title>
+</head>
+<body>
+    <header>
+        <div class="info">
+            <ul class="info-links">
+                <li><a href="#">+639398517336</a></li>
+                <li><a href="#">cityword20@gmail.com</a></li>
+            </ul>
+        </div>
+        <div class="icons">
+            <a href="https://www.facebook.com/CovenantWorkersForChrist"><i id="fb" class="fab fa-facebook-square"></i></a>
+            <a href="https://www.youtube.com/channel/UCH68AOKb2QyEga571FVBQIw"><i id="yt" class="fab fa-youtube"></i></a>
+        </div>
+    </header>
+
+    <nav class="navbar">
+        <div class="logo">
+            <a href="index.php"><img src="logo/Logo.png" id="logo"></a>
+        </div>
+        <ul class="nav-links">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="2-AboutUs.php">About Us</a></li>
+            <li><a href="3-Preachings.php">Preachings</a></li>
+            <li><a href="#">Ministries</a></li>
+            <li><a href="5-Give.php">Give</a></li>
+            <li><a href="6-CnnctWthS.php">Connect with Us</a></li>
+            <li><a href="#">|</a></li>
+            <li><a href="logIn.php">Log-in</a></li>
+        </ul>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>   
+        </div>
+    </nav> 
+
+    <!-- 1 -->
+    <section class="preach">
+        <div class="content">
+                <h4> watch</h4>
+                <iframe width="760" height="400"   
+                    src="https://www.youtube.com/embed/GwZvq5fhqqE" 
+                    title="YouTube video player" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen>
+                </iframe>
+        </div>
+    </section>
+    
+    <!-- 2 -->
+    <section class="channel">
+        <div class="title">
+            <h1>Our Broadcast Channels</h1>
+        <div class="channel-links">
+                <a class = "" href="https://www.facebook.com/CovenantWorkersForChrist" target="_blank">
+                <i class="fab fa-facebook-square"></i>
+                <p class ="">/CovenantWorkersForChrist</p>
+                </a>
+        </div>
+        <div class="channel-links">
+                <a class = "" href="https://www.youtube.com/channel/UCH68AOKb2QyEga571FVBQIw" target="_blank">
+                <i class="fab fa-youtube"></i>
+                <p class ="">City Word Family Church</p>
+                </a>
+        </div>
+        <div class="channel-links">
+            <a class = "" href="https://www.youtube.com/channel/UCH68AOKb2QyEga571FVBQIw" target="_blank">
+            <i class="fab fa-spotify"></i>
+            <p class ="">City Word Family Church</p>
+            </a>
+        </div>
+        </div>
+    </section>
+    
+    <!-- Footer -->
+    <section class="footer">
+    <div class="container flex">
+        <div class="logo">
+            <a href="index.php"><img src="logo/foot-logo.png" id="logo"></a>
+        </div>
+        <div class="foot-links">
+            <div>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="2-AboutUs.php">About Us</a></li>
+                    <li><a href="3-Preachings.php">Preachings</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#">Ministries</a></li>
+                    <li><a href="5-Give.php">Give</a></li>
+                    <li><a href="6-CnnctWthS.php">Connect with Us</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="info">
+            <p> 
+            <i class="fas fa-map-marker-alt"></i> 10th Swallow Street Francisville subd. Mambugan, Antipolo City PH <br> <br>
+            <i class="fas fa-envelope"></i> cityword20@gmail.com <br> <br>
+            <i class="fas fa-phone-alt"></i> (+63) 9398517336 </p>   
+        </div>
+    </div>
+    </section>
+
+        <footer class="foot">
+            <p> City Word Family Church 2022</p>
+        </footer>
+
+    <script src="app.js"></script>
+</body>
+</html>
